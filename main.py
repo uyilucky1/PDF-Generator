f1c7d0f53034916300a528ce99c07852b2084e26
uyilucky1 <uyilucky11@gmail.com>
2023-12-12 14:40:28 +0100

    update

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(10, 100, 10)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set Footer
     pdf.ln(265)
@@ -26,6 +28,8 @@ for index, row in df.iterrows():
         pdf.set_font(family='Times', style='I', size=8)
         pdf.set_text_color(10, 180, 10)
         pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 
 pdf.output('output.pdf')
