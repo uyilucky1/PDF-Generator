90885a7d8e32bb46334198572914c39c2588a87b
uyilucky1 <uyilucky11@gmail.com>
2023-12-12 14:46:23 +0100

    update

--- deploy.py
+++ deploy.py
@@ -14,7 +14,8 @@ def generate_pdf(df):
         pdf.set_font(family='Times', style='B', size=24)
         pdf.set_text_color(10, 100, 10)
         pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-        pdf.line(10, 21, 200, 21)
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # Set Footer
         pdf.ln(265)
@@ -29,6 +30,8 @@ def generate_pdf(df):
             pdf.set_font(family='Times', style='I', size=8)
             pdf.set_text_color(10, 180, 10)
             pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
+            for y in range(20, 298, 10):
+                pdf.line(10, y, 200, y)
 
     return pdf
 
